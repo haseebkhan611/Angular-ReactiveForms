ba65d87792b7dfc82ab60bc3c257ef7628179444
haseebkhan611 <haseebkhan611@users.noreply.github.com>
2021-12-31 10:58:17 +0500

    changes

--- Demo-Start/src/app/customers/customer.component.ts
+++ Demo-Start/src/app/customers/customer.component.ts
@@ -1,8 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { Customer } from './customer';
 
+
+function emailMatcher(c: AbstractControl): {[key: string]: boolean} | null {
+  const emailControl = c.get('email');
+  const confirmControl = c.get('confirmEmail');
+
+  if(emailControl.pristine || confirmControl.pristine){
+    return null;
+  }
+  if(emailControl.value === confirmControl.value) {
+    return null;
+  }
+  return{'match':true};
+}
 @Component({
   selector: 'app-customer',
   templateUrl: './customer.component.html',
@@ -10,14 +23,59 @@ import { Customer } from './customer';
 })
 export class CustomerComponent implements OnInit {
   customer = new Customer();
+  customerForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+
+    this.customerForm=this.fb.group({
+      firstName: ['',[Validators.required,Validators.minLength(3)]],
+      lastName: ['',[Validators.required,Validators.minLength(3)]],
+
+      emailGroup: this.fb.group({
+        email: ['',[Validators.required,Validators.email]],
+        confirmEmail: ['',Validators.required],
+      },{validator:emailMatcher}),
+
+      sendCatalog: [true],
+      phone:[''],
+      notification: 'email'
+    })
+
+    // this.populateTestData()
+    console.log(this.customerForm.controls)
   }
 
-  save(customerForm: NgForm): void {
-    console.log(customerForm.form);
-    console.log('Saved: ' + JSON.stringify(customerForm.value));
+  save() {
+    console.log(this.customerForm);
+    console.log('Saved: ' + JSON.stringify(this.customerForm.value));
   }
+
+  // using this code in ngoninit will initialize the form values
+  populateTestData(): void{
+    this.customerForm.patchValue({
+      firstName: 'Haseeb',
+      lastName: 'Khan',
+      email: 'haseeb@gmail.com',
+      sendCatalog: false
+    })
+  }
+
+
+
+  setNotification(notifyVia: string): void{
+    const phoneControl=this.customerForm.get('phone')
+    if(notifyVia==='text'){
+      phoneControl.setValidators(Validators.required)
+    }else{
+      phoneControl.clearValidators();
+    }
+    phoneControl.updateValueAndValidity();
+
+  }
+
+  
 }
